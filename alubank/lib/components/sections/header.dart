30177213fd607f34f35f33831faba50fbfac2ee2
Pedro Carvalho de Oliveira <pedro.carvalho2806@hotmail.com>
2025-02-10 03:22:29 -0300

    App similar a nubank

---
+++ alubank/lib/components/sections/header.dart
@@ -0,0 +1,54 @@
+import 'package:alubank/themes/theme_colors.dart';
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 180,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: ThemeColors.headerGradient,
+        ),
+        borderRadius: BorderRadius.vertical(bottom: Radius.circular(10)),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text.rich(
+                  TextSpan(
+                    text: '\$',
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: '1000.00',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  'Balanço disponível',
+                ),
+              ],
+            ),
+            Icon(
+              Icons.account_circle,
+              size: 42,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
